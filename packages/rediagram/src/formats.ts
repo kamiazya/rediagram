bbf80917dea527a30211713fa03138e1649d2b42
kamiazya <yuki@kamiazya.tech>
2020-09-29 23:08:20 +0900

    fix(rediagram): fix bug

--- packages/rediagram/src/formats.ts
+++ packages/rediagram/src/formats.ts
@@ -1,5 +1,57 @@
-import { createImageProcessor } from './image-processor';
+import { ReactElement } from 'react';
+import { renderToDot } from '@ts-graphviz/react';
+import { renderDot } from '@ts-graphviz/node';
+import path from 'path';
+import caller from 'caller';
 
-export const PNG = createImageProcessor('png');
-export const SVG = createImageProcessor('svg');
-export const PDF = createImageProcessor('pdf');
+type RenderOption = {
+  dir?: string;
+  name?: string;
+};
+
+export function PNG(element: ReactElement, { name, dir }: RenderOption = {}): void {
+  const p = path.parse(caller());
+  const format = 'png';
+  const output = path.format({
+    dir: dir ?? p.dir,
+    name: name ?? p.name,
+    ext: `.${format}`,
+  });
+  const dot = renderToDot(element);
+  // eslint-disable-next-line no-console
+  console.log(output);
+  renderDot(dot, {
+    format,
+    output,
+  });
+}
+
+export function SVG(element: ReactElement, { name, dir }: RenderOption = {}): void {
+  const p = path.parse(caller());
+  const format = 'svg';
+  const output = path.format({
+    dir: dir ?? p.dir,
+    name: name ?? p.name,
+    ext: `.${format}`,
+  });
+  const dot = renderToDot(element);
+  renderDot(dot, {
+    format,
+    output,
+  });
+}
+
+export function PDF(element: ReactElement, { name, dir }: RenderOption = {}): void {
+  const p = path.parse(caller());
+  const format = 'pdf';
+  const output = path.format({
+    dir: dir ?? p.dir,
+    name: name ?? p.name,
+    ext: `.${format}`,
+  });
+  const dot = renderToDot(element);
+  renderDot(dot, {
+    format,
+    output,
+  });
+}
