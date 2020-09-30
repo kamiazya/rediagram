9dc8c61b31472a56898c1517dc07cf370efc747a
kamiazya <yuki@kamiazya.tech>
2020-10-01 00:28:18 +0900

    docs(@rediagram/website): add Getting Started

--- website/sidebars.js
+++ website/sidebars.js
@@ -1,5 +1,10 @@
 module.exports = {
   someSidebar: {
+    'Getting Started': [
+      'getting-started/getting-started-introduction',
+      'getting-started/getting-started-requirements',
+      'getting-started/getting-started-environment-setup',
+    ],
     rediagram: ['doc1'],
   },
 };
