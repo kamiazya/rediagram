969aada225e5ae629394d4ca34b77380b13bc491
kamiazya <yuki@kamiazya.tech>
2020-10-18 01:55:05 +0900

    fix(create-rediagram-project): fix version, and help option bihabior

--- packages/create-rediagram-project/src/main.ts
+++ packages/create-rediagram-project/src/main.ts
@@ -6,10 +6,12 @@ try {
   command.parse(process.argv);
 } catch (err) {
   if (err instanceof CommanderError) {
-    if (err.code === 'commander.missingArgument') err.code = 'MissingArgument';
-    // eslint-disable-next-line no-console
-    console.log(chalk.red`{bold ${err.code}}\n${err.message}\n`);
-    command.outputHelp();
-    process.exit(err.exitCode);
+    if (!['commander.version', 'commander.helpDisplayed'].includes(err.code)) {
+      if (err.code === 'commander.missingArgument') err.code = 'MissingArgument';
+      // eslint-disable-next-line no-console
+      console.log(chalk.red`{bold ${err.code}}\n${err.message}\n`);
+      command.outputHelp();
+      process.exit(err.exitCode);
+    }
   }
 }
