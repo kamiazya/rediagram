886ab5f5c8b90d297722aed650513cf171eb3df2
kamiazya <yuki@kamiazya.tech>
2020-10-21 19:48:27 +0900

    fix(@rediagram/cli): use @rediagram/common package

--- packages/cli/src/rediagramc.ts
+++ packages/cli/src/rediagramc.ts
@@ -1,5 +1,6 @@
 import cmd from 'commander';
 import glob from 'fast-glob';
+import { CONFIG } from '@rediagram/common';
 import { registerAll } from 'sucrase/dist/register';
 import path from 'path';
 import pkg from './pkg';
@@ -10,11 +11,19 @@ cmd
   .arguments('[pattarns...]')
   .action(async function rediagramc(pattarns: string[]): Promise<void> {
     registerAll();
-    const sources = await glob([...pattarns, '**/*.rediagram.{jsx,tsx}', '!**/node_modules/**/*'], {
-      dot: true,
-      extglob: true,
-      onlyFiles: true,
-    });
+    const sources = await glob(
+      pattarns.length >= 1
+        ? pattarns
+        : [
+            ...(CONFIG.scope.getIncludesPattarns() ?? []),
+            ...(CONFIG.scope.getExcludesPattarns() ?? []).map((p) => `!${p}`),
+          ],
+      {
+        dot: true,
+        extglob: true,
+        onlyFiles: true,
+      },
+    );
     sources.forEach((src) => {
       const resolved = path.resolve(src);
       // eslint-disable-next-line global-require, import/no-dynamic-require
