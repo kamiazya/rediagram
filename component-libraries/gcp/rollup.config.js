95aaf534087aca7c44f677e3e4b4c0d77fe04550
kamiazya <yuki@kamiazya.tech>
2021-07-28 22:53:02 +0900

    chaos: build with rollup

---
+++ component-libraries/gcp/rollup.config.js
@@ -0,0 +1,36 @@
+import typescript from 'rollup-plugin-typescript2';
+import del from 'rollup-plugin-delete';
+import dts from 'rollup-plugin-dts';
+
+/** @type {import('rollup').RollupOptions[]} */
+const options = [
+  {
+    input: './src/index.ts',
+    output: [
+      {
+        format: 'cjs',
+        file: './lib/index.js',
+      },
+    ],
+    external: ['@rediagram/cdk', 'react', '@ts-graphviz/react', 'prop-types', 'path'],
+    plugins: [typescript()],
+  },
+  {
+    input: './lib/index.d.ts',
+    plugins: [
+      del({
+        targets: ['lib/**/*.d.ts', '!lib/index.d.ts'],
+        hook: 'buildEnd',
+      }),
+      dts(),
+    ],
+    output: [
+      {
+        format: 'esm',
+        file: './lib/index.d.ts',
+      },
+    ],
+  },
+];
+
+export default options;
